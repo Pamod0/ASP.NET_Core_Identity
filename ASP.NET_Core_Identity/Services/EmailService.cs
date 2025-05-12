005d59c4bfd2a34d85ddda7100fe8ff6d92518e9
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2025-05-12 09:25:05 +0530

    refactor

--- ASP.NET_Core_Identity/Services/EmailService.cs
+++ ASP.NET_Core_Identity/Services/EmailService.cs
@@ -1,4 +1,5 @@
-﻿using MailKit.Net.Smtp;
+﻿using ASP.NET_Core_Identity.Services.Interfaces;
+using MailKit.Net.Smtp;
 using MailKit.Security;
 using Microsoft.Extensions.Options;
 using MimeKit;
@@ -6,10 +7,6 @@ using MimeKit.Text;
 
 namespace ASP.NET_Core_Identity.Services
 {
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string to, string subject, string body);
-    }
 
     public class EmailService : IEmailService
     {
