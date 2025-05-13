184ed154b130bd41bcdc8a84d3b07c0fa8b3341b
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2025-05-13 16:53:35 +0530

    user controller

---
+++ ASP.NET_Core_Identity/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using ASP.NET_Core_Identity.Data;
+using ASP.NET_Core_Identity.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Core_Identity.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager, ILogger<UserController> logger)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> GetAll()
+        {
+            try
+            {
+                var users = await _userManager.Users
+                    .Select(u => new UserDTO 
+                    {
+                        Id = u.Id,
+                        UserName = u.UserName,
+                        Email = u.Email,
+                        EmailConfirmed = u.EmailConfirmed,
+                        PhoneNumber = u.PhoneNumber,
+                        PhoneNumberConfirmed = u.PhoneNumberConfirmed,
+                        TwoFactorEnabled = u.TwoFactorEnabled,
+                        LockoutEnd = u.LockoutEnd,
+                        LockoutEnabled = u.LockoutEnabled,
+                        AccessFailedCount = u.AccessFailedCount
+                    })
+                    .ToListAsync();
+
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while retrieving users.");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
---
+++ ASP.NET_Core_Identity/DTOs/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace ASP.NET_Core_Identity.DTOs
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public DateTimeOffset? LockoutEnd { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
+    }
+}
